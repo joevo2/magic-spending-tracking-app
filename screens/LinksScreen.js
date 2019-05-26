7df88121270d30473fd78f32c7a9b1c978bb76ce
Joel Yek <joevo2@gmail.com>
2019-05-26 09:15:04 +0800

    Add item screen with price, desc and date picker

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,112 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  DatePickerAndroid,
+  DatePickerIOS,
+  TouchableOpacity
+} from "react-native";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: "Add"
   };
+
+  state = { date: new Date() };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text>Price</Text>
+          <TextInput
+            onChangeText={text => {
+              this.setState({ price: text });
+            }}
+            style={{ backgroundColor: "pink", width: 100 }}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <TextInput
+            onChangeText={text => {
+              this.setState({ desc: text });
+            }}
+            keyboardType="numeric"
+            placeholder="description"
+            style={{ backgroundColor: "pink", flex: 1 }}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <View style={{ flex: 1 }}>
+            {Platform.OS === "ios" ? (
+              <DatePickerIOS
+                date={this.state.date}
+                onDateChange={date => this.setState({ date })}
+              />
+            ) : (
+              <TouchableOpacity
+                onPress={async () => {
+                  const { year, month, day } = await DatePickerAndroid.open({
+                    date: new Date()
+                  });
+                  this.setState({ date: new Date(year, month, day) });
+                }}
+              >
+                <Text>{this.state.date.toString()}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+
+        <View style={styles.tabBarStickyBottom}>
+          <TouchableOpacity
+            onPress={() => {
+              alert(JSON.stringify(this.state));
+            }}
+            style={{ fontWeight: "bold" }}
+          >
+            <Text>Add</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    flex: 1
   },
+  row: {
+    borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10
+  },
+  tabBarStickyBottom: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    }),
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20,
+    justifyContent: "center"
+  }
 });
