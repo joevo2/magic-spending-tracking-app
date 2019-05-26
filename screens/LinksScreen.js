9bd16bf7946b829998e74f1e7a54db7e58f27c23
Joel Yek <joevo2@gmail.com>
2019-05-26 16:28:46 +0800

    upload image to firebase

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -12,6 +12,8 @@ import {
   Image
 } from "react-native";
 
+import uuid from "uuid";
+
 import { Firebase } from "../api/config.js";
 
 import { ImagePicker, Permissions } from "expo";
@@ -21,16 +23,50 @@ export default class LinksScreen extends React.Component {
     title: "Add"
   };
 
-  state = { date: new Date(), image: null };
+  state = { date: new Date(), image: null, imageUploading: false };
 
   handleAddItem = () => {
-    Firebase.database()
-      .ref("users/" + "john")
-      .push({
-        desc: this.state.desc || "",
-        price: this.state.price || 0,
-        date: this.state.date.toLocaleDateString()
-      });
+    if (this.state.imageUploading) {
+      // if image is uploading dont allow
+      alert("image is uploading please wait");
+    } else {
+      Firebase.database()
+        .ref("users/" + "john")
+        .push({
+          desc: this.state.desc || "",
+          price: this.state.price || 0,
+          date: this.state.date.toLocaleDateString(),
+          image: this.state.imageURL
+        });
+    }
+  };
+
+  uploadImageAsync = async uri => {
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function(e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+
+    const ref = Firebase.storage()
+      .ref()
+      .child(uuid.v4());
+    const snapshot = await ref.put(blob);
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
   };
 
   render() {
@@ -92,7 +128,15 @@ export default class LinksScreen extends React.Component {
               });
 
               if (!result.cancelled) {
-                this.setState({ image: result.uri });
+                // show image locally, and prevent user from submitting to firebase before uploading complete
+                this.setState({ image: result.uri, imageUploading: true });
+                try {
+                  // Uploading
+                  const imageURL = await this.uploadImageAsync(result.uri);
+                  this.setState({ imageURL, imageUploading: false });
+                } catch (err) {
+                  console.error(err);
+                }
               }
             }
           }}
