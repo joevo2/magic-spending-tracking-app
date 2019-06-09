0a9e05707c138d4e74675ba43a731d61a63835c6
Joel Yek <joevo2@gmail.com>
2019-06-09 12:56:13 +0800

    fetch api

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+
+import { View, Text, Button } from "react-native";
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: "app.json"
+  };
+
+  state = { weather: null };
+
+  // async componentDidMount() {
+  //   this.setState({
+  //     weather: await fetch(
+  //       "https://www.metaweather.com/api/location/search/?query=london"
+  //     ).then(res => res.json())
+  //   });
+  // }
+
+  getWeather = () => {
+    const location = 'kuala lumpur'
+
+    // const responds = await fetch(
+    //   `https://www.metaweather.com/api/location/search/?query=${location}`
+    // );
+    // const jsonData = await responds.json();
+    // this.setState({
+    //   weather: jsonData
+    // });
+
+
+    fetch("https://www.metaweather.com/api/location/search/?query=" + location)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        this.setState({ weather: json });
+      });
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+        <Text>{JSON.stringify(this.state.weather)}</Text>
+        <Button title="get weather" onPress={this.getWeather} />
+      </View>
+    );
   }
 }
